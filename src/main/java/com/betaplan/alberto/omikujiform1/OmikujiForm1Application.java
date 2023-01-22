77952204c6e6c03dea1dbdc47eae4eff4d226b4e
prendi13 <alberto.prendi19@gmail.com>
2023-01-22 04:18:25 +0100

    omikuji

---
+++ src/main/java/com/betaplan/alberto/omikujiform1/OmikujiForm1Application.java
@@ -0,0 +1,13 @@
+package com.betaplan.alberto.omikujiform1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmikujiForm1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmikujiForm1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/betaplan/alberto/omikujiform1/OmikujiForm1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betaplan.alberto.omikujiform1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmikujiForm1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
